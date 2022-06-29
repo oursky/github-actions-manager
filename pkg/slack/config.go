e500c815eee80bd5b37d1491aabf0cf6b9614ea7
kiootic <kiootic@oursky.com>
2022-06-29 14:05:27 +0800

    Fix disabled slack integration config

--- pkg/slack/config.go
+++ pkg/slack/config.go
@@ -4,8 +4,8 @@ import "github.com/oursky/github-actions-manager/pkg/kv"
 
 type Config struct {
 	Disabled bool   `toml:"disabled"`
-	BotToken string `toml:"botToken" validate:"required"`
-	AppToken string `toml:"appToken" validate:"required"`
+	BotToken string `toml:"botToken" validate:"required_if=Disabled false"`
+	AppToken string `toml:"appToken" validate:"required_if=Disabled false"`
 }
 
 var kvNamespace = kv.RegisterNamespace("slack-subscriptions")
