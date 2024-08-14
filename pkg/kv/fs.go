bffdb47f94464fac49a904ac859614d4960cb9bf
Zachary Tsang <u.quixz@gmail.com>
2024-08-14 10:26:41 +0800

    Remove unneeded mutex and reformat error detection for fs.go

--- pkg/kv/fs.go
+++ pkg/kv/fs.go
@@ -2,24 +2,19 @@ package kv
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"os"
 	"path"
-	"sync"
 
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 )
 
 type FSStore struct {
-	lock   *sync.RWMutex
 	fsPath string
 }
 
 func NewFSStore(logger *zap.Logger, fsPath string) *FSStore {
 	return &FSStore{
-		lock:   new(sync.RWMutex),
 		fsPath: fsPath,
 	}
 }
@@ -27,30 +22,38 @@ func NewFSStore(logger *zap.Logger, fsPath string) *FSStore {
 func (s *FSStore) Start(ctx context.Context, g *errgroup.Group) error {
 	return nil
 }
+
 func (s *FSStore) Get(ctx context.Context, ns Namespace, key string) (string, error) {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
+	path := path.Join(s.fsPath, string(ns), key)
 
-	path := fmt.Sprintf("%s/%s/%s", s.fsPath, ns, key)
 	b, err := os.ReadFile(path)
-
-	if errors.Is(err, os.ErrNotExist) {
-		s.Touch(path)
-		return "", nil
-	}
 	if err != nil {
+		if os.IsNotExist(err) {
+			err := s.Touch(path)
+			if err != nil {
+				return "", err
+			}
+			return "", nil
+		}
 		return "", err
 	}
 	return string(b), nil
 }
 
 func (s *FSStore) Set(ctx context.Context, ns Namespace, key string, value string) error {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+	path := path.Join(s.fsPath, string(ns), key)
 
-	path := fmt.Sprintf("%s/%s/%s", s.fsPath, ns, key)
-	s.Touch(path)
-	return os.WriteFile(path, []byte(value), 0644)
+	err := s.Touch(path)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(path, []byte(value), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *FSStore) Touch(fpath string) error {
@@ -59,9 +62,15 @@ func (s *FSStore) Touch(fpath string) error {
 		return err
 	}
 
-	if file, err := os.OpenFile(fpath, os.O_RDONLY|os.O_CREATE, 0644); err != nil {
+	file, err := os.OpenFile(fpath, os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
 		return err
-	} else {
-		return file.Close()
 	}
+
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
